c1a67073c8edb05738cec773c1af77d5aec673f4
JesseXC <jcruz2@oberlin.edu>
2023-07-05 20:42:59 +0000

    final iteration of  project for week 2

--- project.py
+++ project.py
@@ -14,6 +14,7 @@ class TrendingVideos:
         self.chartedVideos = []
         self.engine = db.create_engine('sqlite:///youtube_most_popular.db')
         self.wiki = wikipediaapi.Wikipedia('en')
+        self.random_regions = []
     
     def store_wiki_info(self):
         wiki_data = []
@@ -56,6 +57,7 @@ class TrendingVideos:
         info["channel"] = video_object.snippet.channelTitle	
         info["description"] = video_object.snippet.localized.description
         info["viewCount"] = video_object.statistics.viewCount
+        info["region"] = self.random_regions[self.chartedVideos.index(video_object)][0]
         info["link"] = f'https://www.youtube.com/watch?v={video_object.id}'
         return info
     
@@ -69,6 +71,7 @@ class TrendingVideos:
             print(f"Channel: {info['channel']}")
             print(f"Description: {info['description']}")
             print(f"View Count: {info['viewCount']}")
+            print(f"Region: {info['region']}")
             print(f"Link: {info['link']}")
             print("-" * 50)
     
@@ -89,17 +92,16 @@ class TrendingVideos:
         for region in response:
             regions.append(region.snippet.gl)
         i = 0
-        random_regions = []
         while i < num_videos:
             choice = random.choices(regions, k=1)
-            if choice in random_regions:
+            if choice in self.random_regions:
                 continue
             else:
-                random_regions.append(choice)
+                self.random_regions.append(choice)
                 i += 1
-        if "US" not in random_regions:
-            random_regions[len(random_regions)-1] = "US"
-        for region in random_regions:
+        if "LY" not in self.random_regions:
+            self.random_regions[len(self.random_regions)-1] = ["LY"]
+        for region in self.random_regions:
             video_by_chart = self.api.get_videos_by_chart(chart="mostPopular", region_code=region, count=1)
             self.chartedVideos.append(video_by_chart.items[0])
         return self.chartedVideos
@@ -130,3 +132,25 @@ while True:
     channel_title = videos[video_number].snippet.channelTitle
     trending.store_wiki_info()
     trending.display_wiki_info(channel_title)
+
+
+    # def display_chart(self, videos):
+    #     print("Trending Videos:")
+    #     print("----------------")
+    #     for i, video in enumerate(videos):
+    #         info = self.get_video_information(video)
+    #         translated_info = self.translate_video_info(info)
+    #         print(f"Video {i+1}:")
+    #         print(f"Title: {translated_info['title']}")
+    #         print(f"Channel: {translated_info['channel']}")
+    #         print(f"Description: {translated_info['description']}")
+    #         print(f"View Count: {translated_info['viewCount']}")
+    #         print(f"Link: {translated_info['link']}")
+    #         print("-" * 50)
+
+    # def translate_video_info(self, info):
+    #     translated_info = {}
+    #     for key, value in info.items():
+    #         translated_value = ts.google(value, to_language='en')
+    #         translated_info[key] = translated_value
+    #     return translated_info
